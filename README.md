0de42692d82a066bac92464bb422e01c853bdf4a
abdessamad-zgor <abdessamadzgor@gmail.com>
2023-01-27 19:31:11 +0100

    Wordle type game

---
+++ README.md
@@ -0,0 +1,9 @@
+# Install dependencies
+
+```
+pip install -r requirements.txt
+```
+# Cheat mode
+```
+python3 main.py --cheat
+```
--- main.py
+++ main.py
@@ -1,87 +1,105 @@
-import art
 import random
-import signal
 import sys
-from typing import List
 from os import system, name, path
+import art
 
 
-def leave(signum, frame):
-    msg = "\nEst-que vous voulez quitter l'application? (y/n) :"
-    print(msg, end="")
-    ch = input()
-    if ch=='y':
-        print("")
-        exit(1)
-    else:
-        print("", end="\r", flush=True)
-        print(" " * len(msg), end="", flush=True)
-        print("    ", end="\r", flush=True)
-
-signal.signal(signal.SIGINT, leave)
-
-def lire_mots()->List[str]:
+def lire_mots():
     with open(path.dirname(__file__)+"/motquiz.txt") as f:
-        mots = [l.strip() for l in f.readlines()]
+        mots = [l.strip() for l in f.readlines() if l!=""]
     return mots
 
-def render(ch:str, mot:str, oldguess:str, attempts: int, win:bool):
-    if(ch==""):
+
+def update_guess(ch: str, oldguess: str, mot: str):
+    if ch in mot and oldguess.count(ch) != mot.count(ch):
+        new = list(oldguess)
+        # trouver la derniere occurance du letter dans la chaine courant
+        try:
+            last_occur = oldguess.rindex(ch)
+            
+        except ValueError:
+            last_occur = -1
+        new[mot.index(ch, 0 if last_occur==-1 else last_occur+1)] = ch
+        new_guess = "".join(new)
+        return new_guess
+    else:
+        return oldguess
+
+
+def render(ch: str, mot: str, oldguess: str, attempts: int, win: bool):
+    if (ch == ""):
         return (oldguess, attempts, False)
-    if(attempts!=0):
-        if ch in mot:
-            new= list(oldguess)
-            new[mot.index(ch)]=ch
-            new_guess = "".join(new)
-            if new_guess == mot:
-                return (new_guess, attempts, True)        
-            
-            return (new_guess, attempts, False)
+    if (attempts != 0):
+        newguess = update_guess(ch, oldguess, mot)
+        if newguess == mot:
+            return (newguess, attempts, True)
+        elif newguess!= oldguess :
+            return (newguess, attempts, False)
         else:
-            
             return (oldguess, attempts-1, False)
     else:
         return ("Game Over", -1, False)
 
+
 def clear():
-    system('clear' if name!="nt" else "cls")
+    system('clear' if name != "nt" else "cls")
+
 
 def game_loop():
-    args = sys.argv
-    mot = random.choice(lire_mots())
-    guess = "_"*len(mot)
+    try:
+        args = sys.argv
+        mot = random.choice(lire_mots())
+        guess = "_"*len(mot)
+        choix = ""
+        attempts = 6
+        win = False
+        while True:
+            clear()
+            (guess, attempts, win) = render(choix, mot, guess, attempts, win)
+            if attempts == -1:
+                print(art.text2art(guess))
+                ch = input("voulez vous rejouir (y/n): ")
+                if ch == 'y':
+                    game_loop()
+                else:
+                    clear()
+                    print(art.text2art("goodbye"))
+                    exit(0)
+            if args.count("--cheat") == 1:
+                print(mot)
+            if win ==True:
+                print(art.text2art("Brovoo!"))
+                print("vous avez deviner le mot   "+mot.upper())
+                ch = input("voulez vous rejouir (y/n): ")
+                if ch == 'y':
+                    game_loop()
+                else:
+                    clear()
+                    print(art.text2art("goodbye"))
+                    exit(0)
 
-    choix=""
-    attempts = 6
-    win = False
-    while True:
-        clear()
-        (guess, attempts, win)=render(choix, mot, guess, attempts, win)
-        if attempts==-1:
+            print(f"\t\t\t\t\t\t\t\t\t il vous reste {attempts} essais")
             print(art.text2art(guess))
-            ch = input("voulez vous rejouir (y/n): ")
-            if ch=='y':
-                game_loop()
-            else:
-                exit(0)
-        if args.count("--cheat")==1:
-            print(mot)
-        if win:
-            print(art.text2art("Brovoo!"))
-            
+            print("Entrez un character : ", end="")
+            while True:
+                choix = input()
+                if len(choix)!=1:
+                    print("Entrez une seul character : ", end="")
+                else:
+                    break;
+    except KeyboardInterrupt:
+        print()
+        ch = input("Voulez vous quitez l'application (y/n) : ")
+        if ch=='y':
+            clear()
+            print(art.text2art("goodbye"))
+            exit(0)
+        else:
+            game_loop()
 
-
-
-        print(f"\t\t\t\t\t\t\t\t\t\t\t\t il vous reste {attempts} essais")
-        print(art.text2art(guess))
-        print("Entrez un character : ", end="")
-        choix= input()
-    
 
 def main():
     game_loop()
 
-main()
 
-    
-        
+main()
